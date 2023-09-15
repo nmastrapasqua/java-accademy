7c9f717059862946f852e27adafe25e708d185e0
Nunzio Mastrapasqua <nunzio.mastrapasqua@sideagroup.com>
2023-09-15 17:23:36 +0200

    first commit

---
+++ labs/maven/mavenintro/src/main/java/org/example/Main.java
@@ -0,0 +1,10 @@
+package org.example;
+
+import org.joda.time.LocalTime;
+
+public class Main {
+    public static void main(String[] args) {
+        LocalTime currentTime = new LocalTime();
+        System.out.println("The current local time is: " + currentTime);
+    }
+}
