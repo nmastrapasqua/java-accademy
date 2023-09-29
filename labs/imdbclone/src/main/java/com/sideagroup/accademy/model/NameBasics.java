35bd1f2048a0cc2e7152d9815833c1e3a173ab6f
Nunzio Mastrapasqua <nunzio.mastrapasqua@sideagroup.com>
2023-09-29 12:34:29 +0200

    Step 5

---
+++ labs/imdbclone/src/main/java/com/sideagroup/accademy/model/NameBasics.java
@@ -0,0 +1,55 @@
+package com.sideagroup.accademy.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="name_basics")
+public class NameBasics {
+    @Id
+    @Column(name = "nconst", length = 200, nullable = false)
+    private String id;
+
+    @Column(length = 1000, nullable = false)
+    private String primaryName;
+
+    @Column(nullable = false)
+    private Integer birthYear;
+
+    @Column(nullable = false)
+    private Integer deathYear;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getPrimaryName() {
+        return primaryName;
+    }
+
+    public void setPrimaryName(String primaryName) {
+        this.primaryName = primaryName;
+    }
+
+    public Integer getBirthYear() {
+        return birthYear;
+    }
+
+    public void setBirthYear(Integer birthYear) {
+        this.birthYear = birthYear;
+    }
+
+    public Integer getDeathYear() {
+        return deathYear;
+    }
+
+    public void setDeathYear(Integer deathYear) {
+        this.deathYear = deathYear;
+    }
+}
