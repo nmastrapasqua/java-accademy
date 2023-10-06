0b1c6d2cc990a2eeb7c1702053efc7d89aea8337
Nunzio Mastrapasqua <nunzio.mastrapasqua@sideagroup.com>
2023-10-06 14:54:10 +0200

    Step 9

--- labs/imdbclone/src/main/java/com/sideagroup/accademy/controller/api/CelebrityController.java
+++ labs/imdbclone/src/main/java/com/sideagroup/accademy/controller/api/CelebrityController.java
@@ -1,9 +1,15 @@
 package com.sideagroup.accademy.controller.api;
 
 import com.sideagroup.accademy.dto.CelebrityDto;
+import com.sideagroup.accademy.dto.DefaultErrorDto;
 import com.sideagroup.accademy.dto.GetAllCelebritiesResponseDto;
 import com.sideagroup.accademy.exception.GenericServiceException;
 import com.sideagroup.accademy.service.CelebrityService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -19,6 +25,15 @@ public class CelebrityController {
     @Autowired
     private CelebrityService service;
 
+    @Operation(summary = "Retrieves all celebrities without movies",
+            description = "Retrieves all celebrities without movies in paginated way")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Successfully retrieved"),
+            @ApiResponse(
+                    responseCode = "400",
+                    description = "One or more parameters are invalid",
+                    content = @Content(schema = @Schema(implementation = DefaultErrorDto.class)))
+    })
     @GetMapping
     public GetAllCelebritiesResponseDto getAll(
             @RequestParam(name="page", required=false, defaultValue="0") int page,
@@ -32,6 +47,15 @@ public class CelebrityController {
         }
     }
 
+    @Operation(summary = "Gets a celebrity with movies by id",
+            description = "Returns a celebrity with movies as per the id")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Successfully retrieved"),
+            @ApiResponse(
+                    responseCode = "404",
+                    description = "Not found - The celebrity was not found",
+                    content = @Content(schema = @Schema(implementation = DefaultErrorDto.class)))
+    })
     @GetMapping("{id}")
     public CelebrityDto getById(@PathVariable String id) {
         Optional<CelebrityDto> opt = service.getById(id);
@@ -42,6 +66,14 @@ public class CelebrityController {
         return opt.get();
     }
 
+    @Operation(summary = "Creates a new celebrity", description = "Creates a new celebrity")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Successfully created"),
+            @ApiResponse(
+                    responseCode = "404",
+                    description = "A celebrity with same id already exists",
+                    content = @Content(schema = @Schema(implementation = DefaultErrorDto.class)))
+    })
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public CelebrityDto create(@RequestBody CelebrityDto celebrity) {
@@ -53,6 +85,14 @@ public class CelebrityController {
         }
     }
 
+    @Operation(summary = "Updates a celebrity", description = "Updates the celebrity with the given id")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Successfully updated"),
+            @ApiResponse(
+                    responseCode = "404",
+                    description = "Not found - The celebrity was not found",
+                    content = @Content(schema = @Schema(implementation = DefaultErrorDto.class)))
+    })
     @PutMapping("{id}")
     public CelebrityDto update(@PathVariable String id, @RequestBody CelebrityDto celebrity) {
         Optional<CelebrityDto> opt = service.update(id, celebrity);
@@ -63,6 +103,10 @@ public class CelebrityController {
         return opt.get();
     }
 
+    @Operation(summary = "Deletes a celebrity", description = "Deletes the celebrity with the given id")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "204", description = "Successfully deleted")
+    })
     @DeleteMapping("{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void deleteById(@PathVariable String id) {
--- labs/imdbclone/src/main/java/com/sideagroup/accademy/dto/CelebrityDto.java
+++ labs/imdbclone/src/main/java/com/sideagroup/accademy/dto/CelebrityDto.java
@@ -1,5 +1,6 @@
 package com.sideagroup.accademy.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -9,10 +10,14 @@ import java.util.List;
 @Getter
 @Setter
 public class CelebrityDto {
+    @Schema(description = "Celebrity Id", example = "nm0089217", required = true)
     private String id;
+    @Schema(description = "Celebrity full name", example = "Orlando Bloom", required = true)
     private String name;
+    @Schema(description = "Year of birth", example = "1977")
     private Integer birthYear;
     private Integer deathYear;
+    @Schema(description = "List of movie for this celebrity. Not used in POST and PUT")
     private List<MovieCelebrityDto> movies;
 
     public CelebrityDto() {
